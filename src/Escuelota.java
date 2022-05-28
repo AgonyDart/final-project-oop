77e94f466e517b0bd145d0f4244851765c94e8cb
JSI3L <105135942+JSI3L@users.noreply.github.com>
2022-05-27 23:50:19 -0500

    Update Escuelota.java

--- src/Escuelota.java
+++ src/Escuelota.java
@@ -4,6 +4,7 @@ public class Escuelota {
     private String nombre;
     private String director;
     private String web;
+    private Persona personas[] = new Persona[1000];
 
     public Escuelota(){}
 
